9ed25c24402be66caf9c9fcda17993b14b6a7a57
Isaiah C <95327043+theisaiahcc@users.noreply.github.com>
2023-02-21 13:25:35 -0800

    Added DbContext class

---
+++ eCommerceSite/Data/GameContext.cs
@@ -0,0 +1,16 @@
+﻿using eCommerceSite.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerceSite.Data
+{
+    public class GameContext : DbContext
+    {
+        public GameContext(DbContextOptions<GameContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Game> Games { get; set; }
+
+    }
+}
