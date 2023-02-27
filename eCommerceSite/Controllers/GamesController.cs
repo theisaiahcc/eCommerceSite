d09e03161e6eab3190325e0eeccec939377680f7
Isaiah C <95327043+theisaiahcc@users.noreply.github.com>
2023-02-27 14:19:06 -0800

    made create game view

---
+++ eCommerceSite/Controllers/GamesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerceSite.Controllers
+{
+    public class GamesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        /*public IActionResult Index()
+        {
+            return View();
+        }*/
+    }
+}
