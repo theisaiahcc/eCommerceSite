f70b2b532ee3a368ca4ba4058acb54ccb923cb2b
Isaiah C <95327043+theisaiahcc@users.noreply.github.com>
2023-02-27 14:52:50 -0800

    Create game fully functional, added styling

--- eCommerceSite/Controllers/GamesController.cs
+++ eCommerceSite/Controllers/GamesController.cs
@@ -1,15 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eCommerceSite.Data;
+using eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace eCommerceSite.Controllers
 {
     public class GamesController : Controller
     {
+        private readonly GameContext _context;
+        public GamesController(GameContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public IActionResult Create()
         {
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Create(Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                // Add to DB
+                _context.Games.Add(game); // prepares insert
+                _context.SaveChanges();   // executes insert
+                // Show success message
+                ViewData["Message"] = $"{game.Title} was added successfully.";
+                return View();
+            }
+            return View(game);
+        }
         /*public IActionResult Index()
         {
             return View();
