f4cc66277d192dff8595e788bf399d5c6ad93ee4
Isaiah C <95327043+theisaiahcc@users.noreply.github.com>
2023-03-07 17:43:31 -0800

    Add member class

--- eCommerceSite/Data/GameContext.cs
+++ eCommerceSite/Data/GameContext.cs
@@ -11,5 +11,7 @@ namespace eCommerceSite.Data
         }
 
         public DbSet<Game> Games { get; set; }
+
+        public DbSet<Member> Members { get; set; }
     }
 }
---
+++ eCommerceSite/Models/Member.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eCommerceSite.Models
+{
+    public class Member
+    {
+        [Key]
+        public int MemberId { get; set; }
+
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+        public string Username { get; set; }
+    }
+}
