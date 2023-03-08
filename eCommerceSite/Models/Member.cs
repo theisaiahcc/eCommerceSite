2a840bd11443518f033efb1384748a66204f9340
Isaiah C <95327043+theisaiahcc@users.noreply.github.com>
2023-03-07 18:01:31 -0800

    Added registerViewModel in Member and added MembersController

--- eCommerceSite/Models/Member.cs
+++ eCommerceSite/Models/Member.cs
@@ -12,4 +12,22 @@ namespace eCommerceSite.Models
         public string Phone { get; set; }
         public string Username { get; set; }
     }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [Compare(nameof(Email))]
+        [Display(Name  = "Confirm Email")]
+        public string ConfirmEmail { get; set; }
+        [Required]
+        [StringLength(75)]
+        public string Password { get; set; }
+        [Required]
+        [Compare(nameof(Password))]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+    }
 }
